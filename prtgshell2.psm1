04c81075b0a64621cc6ef72d2a4ac4d077ebd196
sandersjds <josh@jsanders.us>
2014-02-17 13:54:14 -0500

    Added get-prtgobject
    basic outline, currently returns raw xmlnode

--- prtgshell2.psm1
+++ prtgshell2.psm1
@@ -988,6 +988,110 @@ function Get-PrtgTableData {
 	}
 }
 
+
+
+
+function Get-PrtgObject {
+
+	Param (
+		[Parameter(Mandatory=$false,Position=0)]
+		[int]$ObjectId = 0
+	)
+
+	BEGIN {
+		if ($PRTG.Protocol -eq "https") { $PRTG.OverrideValidation() }
+	}
+
+	PROCESS {
+
+		$Parameters = @{
+			"content" = "sensortree"
+			"id" = $ObjectId
+		}
+		
+		$url = $PrtgServerObject.UrlBuilder("api/table.xml",$Parameters)
+
+		##### data returned; do!
+
+		if ($Raw) {
+			$QueryObject = HelperHTTPQuery $url
+			return $QueryObject.Data
+		}
+
+		$QueryObject = HelperHTTPQuery $url -AsXML
+		$Data = $QueryObject.Data
+
+		$DeviceType = $Data.prtg.sensortree.nodes.SelectNodes("*[1]").LocalName
+		
+		return $Data.prtg.sensortree.nodes.SelectNodes("*[1]")
+		
+		$ReturnData = @()
+		
+		<#
+		
+		HOW THIS WILL LIKELY NEED TO WORK
+		---
+		
+		build a switch statement that uses $Content to determine which types of objects we're going to create
+		foreach item, assign all properties to the object
+		attach the object to $ReturnData
+		
+		#>
+		
+		$PrtgObjectType = switch ($Content) {
+			"probes"	{ "PrtgShell.PrtgProbe" }
+			"groups"	{ "PrtgShell.PrtgGroup" }
+			"devices"	{ "PrtgShell.PrtgDevice" }
+			"sensors"	{ "PrtgShell.PrtgSensor" }
+			"todos"		{ "PrtgShell.PrtgTodo" }
+			"messages"	{ "PrtgShell.PrtgMessage" }
+			"values"	{ "PrtgShell.PrtgValue" }
+			"channels"	{ "PrtgShell.PrtgChannel" }
+			"history"	{ "PrtgShell.PrtgHistory" }
+		}
+		
+		
+		
+
+		foreach ($item in $Data.$Content.item) {
+			$ThisObject = New-Object $PrtgObjectType
+			#$ThisRow = "" | Select-Object $SelectedColumns
+			foreach ($Prop in $SelectedColumns) {
+				if ($Content -eq "channels" -and $Prop -eq "lastvalue_raw") {
+					# fix a bizarre formatting bug
+					#$ThisObject.$Prop = HelperFormatHandler $item.$Prop
+					$ThisObject.$Prop = $item.$Prop
+				} elseif ($HTMLColumns -contains $Prop) {
+					# strip HTML, leave bare text
+					$ThisObject.$Prop =  $item.$Prop -replace "<[^>]*?>|<[^>]*>", ""
+				} else {
+					$ThisObject.$Prop = $item.$Prop
+				}
+			}
+			$ReturnData += $ThisObject
+		}
+
+		if ($ReturnData.name -eq "Item" -or (!($ReturnData.ToString()))) {
+			$DeterminedObjectType = Get-PrtgObjectType $ObjectId
+
+			$ValidQueriesTable = @{
+				group=@("devices","groups","sensors","todos","messages","values","history")
+				probenode=@("devices","groups","sensors","todos","messages","values","history")
+				device=@("sensors","todos","messages","values","history")
+				sensor=@("messages","values","channels","history")
+				report=@("Currently unsupported")
+				map=@("Currently unsupported")
+				storedreport=@("Currently unsupported")
+			}
+
+			Write-Host "No $Content; Object $ObjectId is type $DeterminedObjectType"
+			Write-Host (" Valid query types: " + ($ValidQueriesTable.$DeterminedObjectType -join ", "))
+		} else {
+			return $ReturnData
+		}
+	}
+}
+
 ###############################################################################
 
 function New-PrtgSensor {
