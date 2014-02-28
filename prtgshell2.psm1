0e04a0943b701c201f30fb4bcdf6de4d9b198fac
brianaddicks <brian@addicks.us>
2014-02-27 20:58:52 -0500

    added set-prtgerror cmdlet

--- prtgshell2.psm1
+++ prtgshell2.psm1
@@ -646,6 +646,20 @@ function New-PrtgSensor {
 
 ###############################################################################
 
+function Set-PrtgError {
+  Param (
+    [Parameter(Mandatory=$True,Position=0)]
+    [string]$ErrorText
+  )
+
+  PROCESS {
+    $XmlObject = New-Object PrtgShell.ExeXml
+    return $XmlObject.PrintError($ErrorText)
+  }
+}
+
+###############################################################################
+
 function New-PrtgResult {
     <#
 		.SYNOPSIS
